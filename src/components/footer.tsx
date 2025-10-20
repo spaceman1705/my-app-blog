eb2ee3fb7792d82a477acb1a16bbc82cf9aeed9a
spaceman1705 <awangdota2@gmail.com>
2025-10-20 12:11:25 +0700

    updt1

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -50,7 +50,7 @@ export default function Footer() {
         </div>
       </div>
       <div className="border-t border-gray-700 mt-12 pt-6 text-center text-sm text-gray-500">
-        © {new Date().getFullYear()} Taroto Dessert. All rights reserved.
+        © {new Date().getFullYear()}.
       </div>
     </footer>
   );
